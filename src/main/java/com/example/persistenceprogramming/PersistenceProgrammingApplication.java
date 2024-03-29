311505676f06d09ce6813a32f81f89b4184f1608
ahn-sj <jaesa5221@gmail.com>
2024-03-29 22:09:04 +0900

    Init Project

---
+++ src/main/java/com/example/persistenceprogramming/PersistenceProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistenceprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/persistenceprogramming/PersistenceProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persistenceprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
